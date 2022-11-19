1c937719ba92391123391d4c25fe3c2fa56e871c
Chittoji pravin Ramanayya <chittojiprr@birlasoft.com>
2022-11-20 00:33:54 +0530

    RR

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
 import { ResourceRequisition } from './Component/ResourceRequisition';
-import { ResourceRequisition1 } from './Component/ResourceRequisition1';
 
 function App() {
   return (
     <div className="App">
-      {/* <ResourceRequisition/> */}
-      <ResourceRequisition1/>
+      <ResourceRequisition/>
     </div>
   );
 }
